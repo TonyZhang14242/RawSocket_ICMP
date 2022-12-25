a7b631c8c59bd9428d1ee4d5f94119cd629a6e00
ZHANG Zhentao <95693163+TonyZhang14242@users.noreply.github.com>
2022-12-25 19:02:18 +0800

    Update Test Case in case of confusion in checksum

--- tests/ping_test.py
+++ tests/ping_test.py
@@ -1,6 +1,8 @@
+import sys
 import unittest
 
 
+sys.path.append("..")
 from models import ICMPRequest
 from ping import ping
 from sockets import ICMPSocket
@@ -14,26 +16,26 @@ class PingTest(unittest.TestCase):
 	def tearDownClass(cls):
 		cls.socket.close()
 	def testCheckSum_basic(self):
-		ans = self.socket._checksum('test'.encode())
-		self.assertEqual(6182,ans)
-	def testCheckSum_with_header(self):
-		ans = self.socket._checksum(b'\x08\x00\x00\x01\x00\x01\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70\x71\x72\x73\x74\x75\x76\x77\x61\x62\x63\x64\x65\x66\x67\x68\x69')
-		self.assertEqual(19802,ans)
+		ans = self.socket._checksum(b'\x08\x00\x00\x00\x04\x00\x00\x01abcd')
+		self.assertEqual(12088,ans)
+	def testCheckSum_with_random_param(self):
+		ans = self.socket._checksum(b'\x08\x00\x00\x00\x03\x81\x00\x01kdzqdOFsgHIK60OuuSEwNo7ps7v7P78mRCLjg5dVw3IzPu58PD25ADtL')
+		self.assertEqual(37455,ans)
 	def testCheckData_true(self):
-		ans = self.socket._check_data(b'\x08\x00\x00\x01\x00\x01\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70\x71\x72\x73\x74\x75\x76\x77\x61\x62\x63\x64\x65\x66\x67\x68\x69',19802)
+		ans = self.socket._check_data(b'\x08\x00\x00\x00E9\x00\x01zIFGqgWafHAmyLx4q7370PGW4S6633gAKMJWzqW1H9JA5w0E1FiGZdD3',54909)
 		self.assertTrue(ans)
 	def testCheckData_false(self):
-		ans = self.socket._check_data(b'\x08\x00\x00\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70\x71\x72\x73\x74\x75\x76\x77\x61\x62\x63\x64\x65\x66\x67\x68\x69',19802)
+		ans = self.socket._check_data(b'\x08\x00\x00\x00E9\x00\x01zIFGqgWafHAmyLx4q7370PGW4S661H9JA5w0E1asfasfgdsasFiGZdD3',54909)
 		self.assertFalse(ans)
 	def testCreatePacket(self):
 		request = ICMPRequest(
 			destination='223.5.5.5',
 			id=13009,
-			sequence=0,
+			sequence=1,
 			payload=b'AAAA'
 		)
 		ans = self.socket._create_packet(request)
-		self.assertEqual(b'\x08\x00B\xac2\xd1\x00\x00AAAA', ans)
+		self.assertEqual(b'\x08\x00B\xab2\xd1\x00\x01AAAA', ans)
 	def testParsePacket_normal(self):
 		reply = self.socket._parse_reply(b'E\x00\x00T.d\x00\x00/\x01\xad\xdd\xb2\x9d:\xbb\xc0\xa8\x01g\x00\x00\xdcKJ\x95\x00\x00GoQOrJrWZA5V82mrPkOtQrjXf07EZeNdYZkwNZ7ReHx7kwzlowcfArbf','223.5.5.5',1669111491.8911805)
 		self.assertEqual(19093,reply.id)
